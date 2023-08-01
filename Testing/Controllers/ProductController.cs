6dfb3d35a5f0833416d38ec2189d22617639c7e3
eopong87 <eopong87@gmail.com>
2023-08-01 12:39:29 -0400

    exercise 2

--- Testing/Controllers/ProductController.cs
+++ Testing/Controllers/ProductController.cs
@@ -18,18 +18,17 @@ namespace Testing.Controllers
 
         }
 
-
-
-
-
-
-
-
         // GET: /<controller>/
         public IActionResult Index()
         {
             var products = repo.GetAllProducts();
             return View(products);
+        }
+
+        public IActionResult ViewProduct(int id)
+        {
+            var product = repo.GetProduct(id);
+            return View(product);
         }
     }
 }
