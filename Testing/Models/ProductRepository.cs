af8b3d237fcaf6fa9d1f0e8b42a0a05f5787e844
eopong87 <eopong87@gmail.com>
2023-08-01 13:10:07 -0400

    exercise 3

--- Testing/Models/ProductRepository.cs
+++ Testing/Models/ProductRepository.cs
@@ -5,13 +5,13 @@ using Dapper;
 
 namespace Testing.Models
 {
-	public class ProductRepository:IProductRepository
-	{
+    public class ProductRepository : IProductRepository
+    {
 
         private readonly IDbConnection _conn;
 
         public ProductRepository(IDbConnection conn)
-		{
+        {
             _conn = conn;
         }
 
@@ -24,7 +24,12 @@ namespace Testing.Models
             return _conn.QuerySingle<Product>("SELECT * FROM PRODUCTS WHERE PRODUCTID = @id", new { id = id });
         }
 
+        public void UpdateProduct(Product product)
+        {
+            _conn.Execute("UPDATE products SET Name = @name, Price = @price WHERE ProductID = @id",
+             new { name = product.Name, price = product.Price, id = product.ProductID });
+        }
+
     }
-    
 }
 
